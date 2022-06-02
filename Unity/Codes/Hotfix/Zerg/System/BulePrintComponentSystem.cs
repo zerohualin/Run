207a27bd092d362627fdd41e9454c0a016c32d81
Hualin <286990256@qq.com>
2022-06-03 00:26:36 +0800

    BluePrintComponent

---
+++ Unity/Codes/Hotfix/Zerg/System/BulePrintComponentSystem.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using Cfg.zerg;
+
+namespace ET
+{
+    public static class BulePrintComponentSystem
+    {
+        public static bool CheckAlreadyHave(this BulePrintComponent self, int bluePrintId)
+        {
+            if (self.BluePrintIdList.Contains(bluePrintId))
+            {
+                Log.Error("已经拥有改蓝图了！");
+                return true;
+            }
+            return false;
+        }
+        public static void AddBluePrint(this BulePrintComponent self, int bluePrintId)
+        {
+            if (!self.CheckAlreadyHave(bluePrintId))
+            {
+                self.BluePrintIdList.Add(bluePrintId);
+            }
+        }
+
+        public static List<BluePrintConfig> GetAllBluePrint;
+
+    }
+}
---
+++ Unity/Codes/Model/Zerg/BulePrintComponent.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace ET
+{
+    public class BulePrintComponent : Entity
+    {
+        public List<int> BluePrintIdList = new List<int>();
+    }
+}
